efa1af77f32013835cb3918e103bfa4131a7350a
esmicullen <83991065+esmicullen@users.noreply.github.com>
2022-01-06 11:49:00 +0900

    Add files via upload

---
+++ JAVA_B_DAY4/src/test/t1/AAA.java
@@ -0,0 +1,9 @@
+package test.t1;
+
+public class AAA {
+	
+	public void accessTest() {
+		MainTest mt = new MainTest();
+		
+	}
+}
---
+++ JAVA_B_DAY4/src/test/t2/BBB.java
@@ -0,0 +1,9 @@
+package test.t2;
+
+import test.t1.MainTest;
+
+public class BBB {
+	public void accessTest() {
+		MainTest mt = new MainTest();
+	}
+}
